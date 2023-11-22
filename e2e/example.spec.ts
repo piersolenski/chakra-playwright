c7aa9b158d26e2c810927ec1edfc5222db28396f
Piers Olenski <hello@piers.io>
2023-11-22 15:49:37 +0000

    Add sample spec

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,14 +1,8 @@
-import { test, expect } from '@playwright/test'
-
-test('should navigate to the about page', async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('/')
-  // Find an element with the text 'About Page' and click on it
-  await page.getByText('About Page').click()
-  // The new url should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL('/about')
-  // The new page should contain an h1 with "About Page"
-  await expect(page.getByRole('heading', { level: 1 })).toContainText(
-    'About Page'
-  )
-})
+test("should navigate to the Installation page", async ({ page }) => {
+  await page.goto("https://chakra-ui.com");
+  await page.click('[href="/getting-started"]');
+  await expect(page).toHaveURL("https://chakra-ui.com/getting-started");
+  await expect(page.getByRole("heading", { level: 1 })).toContainText(
+    "Installation",
+  );
+});
